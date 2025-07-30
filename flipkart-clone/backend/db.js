1d5d04df02c01637e910ac8d7fe6830849b019c2
Rimi2910 <trishad257@gmail.com>
2025-07-30 22:40:36 +0530

    Add files via upload

---
+++ flipkart-clone/backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb+srv://trishad257:redmiprime8212@cluster0.xsogjg3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected successfully"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
